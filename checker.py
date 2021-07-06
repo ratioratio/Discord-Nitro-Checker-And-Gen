f4d3289fbe0b6f0b10748ddc9d198079a921cdaf
yourpalawsome <83388579+yourpalawsome@users.noreply.github.com>
2021-07-06 00:07:11 -0600

    Update checker.py
    updated ui bc old one looked like shit

--- checker.py
+++ checker.py
@@ -2,30 +2,29 @@ import random, string
 import webbrowser
 import time
 import requests
+import colorama
 import ctypes
 from os import system
+from colorama import Fore, Back, Style
+colorama.init()
 
-
+ctypes.windll.kernel32.SetConsoleTitleW("Nitro Checker")
+print(f"{Fore.GREEN}")
 print("""
-╔═╗░╔╗╔╗░░░░░░░░░░░░░░░░░░░░░░░░░
-║║╚╗║╠╝╚╗░░░░░░░░░░░░░░░░░░░░░░░░
-║╔╗╚╝╠╗╔╬═╦══╗░░░░░░░░░░░░░░░░░░░
-║║╚╗║╠╣║║╔╣╔╗╠══╦══╦══╦══╦══╦══╗░
-║║░║║║║╚╣║║╚╝╠══╩══╩══╩══╩══╩══╝░
-╚╝░╚═╩╩═╩╝╚══╝░░░░░░░░░░░░░░░░░░░
-╔═══╦╗░░░░░░░╔╗░░░░░░░░░░░░░░░░░░
-║╔═╗║║░░░░░░░║║░░░░░░░░░░░░░░░░░░
-║║░╚╣╚═╦══╦══╣║╔╦══╦═╗░░░░░░░░░░░
-║║░╔╣╔╗║║═╣╔═╣╚╝╣║═╣╔╩═╦══╦══╦══╗
-║╚═╝║║║║║═╣╚═╣╔╗╣║═╣╠══╩══╩══╩══╝
-╚═══╩╝╚╩══╩══╩╝╚╩══╩╝░░░░░░░░░░░░
-
+  _   _ _ _                _____ _               _             
+ | \ | (_) |              / ____| |             | |            
+ |  \| |_| |_ _ __ ___   | |    | |__   ___  ___| | _____ _ __ 
+ | . ` | | __| '__/ _ \  | |    | '_ \ / _ \/ __| |/ / _ \ '__|
+ | |\  | | |_| | | (_) | | |____| | | |  __/ (__|   <  __/ |   
+ |_| \_|_|\__|_|  \___/   \_____|_| |_|\___|\___|_|\_\___|_|   
+                                                               
+                                                               
 
 """)
 time.sleep(2)
-print("made by awsome#0001")
+print("Nitro Code Checker")
 time.sleep(1)
-input("press any key to start checking:")
+input("Press Enter To Check Codes.txt: ")
 
 
 with open("codes.txt") as f:
@@ -37,12 +36,12 @@ with open("codes.txt") as f:
         r = requests.get(url)
 
         if r.status_code == 200:
-            print(" Valid | {} ".format(line.strip("\n")))
+            print(" Valid Code: {} ".format(line.strip("\n")))
             break
         else:
-        	print(" Invalid | {} ".format(line.strip("\n")))
-input("The end! Press Enter 5 times to close the program.")
-input("4")
-input("3")
-input("2")
-input("1")
+        	print(Fore.RED + "Invalid Code L {} ".format(line.strip("\n")))
+
+time.sleep(1)
+print("codes checked!")
+time.sleep(2)
+input("finished checking, press enter to close")
